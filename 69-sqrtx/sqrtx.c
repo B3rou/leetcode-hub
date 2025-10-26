4a1f4e27f77714c9f9c767d411adf01669eb50d1
Berou <beratkolip@gmail.com>
2025-10-26 23:16:00 +0300

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.85%) - LeetSync

--- 69-sqrtx/sqrtx.c
+++ 69-sqrtx/sqrtx.c
@@ -1,14 +1,21 @@
 int mySqrt(int x) {
-    long long i, testroot;
-    for (i = 0; i <= x; i++) {
-        testroot = i*i;
-        if (testroot > x)
-        {
-            return (i - 1);
+    if (x == 0 || x == 1)
+        return x;
+
+    long long left = 1, right = x, ans = 0;
+
+    while (left <= right) {
+        long long mid = (left + right) / 2;
+
+        if (mid * mid == x)
+            return mid;
+        else if (mid * mid < x) {
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    if (x == 1){
-        return(1);
-    }
-    return(0);
+
+    return ans;
 }
