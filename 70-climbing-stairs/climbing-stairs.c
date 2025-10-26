a3200992b5eb54cfef6310ac6d6e7fda554b6aaf
b3rou <222520953+B3rou@users.noreply.github.com>
2025-10-26 23:55:26 +0300

    Time: 0 ms (100.00%) | Memory: 7.7 MB (62.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.c
+++ 70-climbing-stairs/climbing-stairs.c
@@ -1,12 +1,12 @@
 int climbStairs(int n) {
-    if (n == 1) return 1;
+     if (n == 1) return 1;
     if (n == 2) return 2;
-    int i;
-    long long stairs[n+1];
-    stairs[1] = 1, stairs[2] = 2;
-    for (i = 3; i <= n; i++){
-        stairs[i] = stairs[i-1] + stairs[i-2];
-    }
 
-    return(stairs[n]);
+    long long a = 1, b = 2, c;
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
