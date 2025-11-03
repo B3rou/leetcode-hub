5a9fb349d7284bbe41b479f11db2621ebe64ab0b
Berou <beratkolip@gmail.com>
2025-11-03 19:06:28 +0300

    Time: 349 ms (5.15%) | Memory: 9 MB (31.09%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,7 +1,17 @@
 int missingNumber(int* nums, int numsSize) {
-    int i, sum = 0;
-    for (i = 0; i < numsSize; i++){
-        sum += nums[i];
+    int i,j,encounter = 0;
+    for (i = 1; i <= numsSize; i++){
+        // counting one by one and going to check its match
+        for (j = 0; j < numsSize; j++){
+            // will read all indexes one by one
+            if (nums[j] == i){
+                encounter = 1;
+                break;
+            }
+            if (encounter == 0 && j == numsSize - 1)
+                return (i);
+        }
+        encounter = 0;
     }
-    return((numsSize*(numsSize + 1)/2) - sum);
+    return(0);
 }
